0d540cadcb5497a2818e5fc0e17e715d22a0a994
nikita1803 <rainikita595@gmail.com>
2021-05-29 23:20:45 +0530

    Added: Analyse Mood through exception

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -21,4 +21,12 @@ public class MoodAnalyser {
         String mood = moodAnalyser.analyseMood("Hello i am Happy");
         Assert.assertEquals("HAPPY",mood);
     }
+	@Test
+    public void givenNullMassageShouldReturnHappyMessage()
+	{
+        MoodAnalyserTest moodAnalyzer = new MoodAnalyserTest(null);
+        String mood = moodAnalyzer.exceptionAnalyseMood();
+        Assert.assertEquals("HAPPY",mood);
+
+    }
 }
--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -2,15 +2,51 @@ package moodanalyser;
 
 public class MoodAnalyserTest 
 {
+	public String message;
 	/**
 	 * analyseMood is a method to check if sad message
 	 * then contain sad if happy then return happy
 	 * @param message
 	 * @return string value
 	 */
-	public String analyseMood(String message) {
+	public String analyseMood(String message) 
+	{
         if(message.contains("Sad"))
             return "SAD";
         return "HAPPY";
+    }
+	/**
+	 * Parameterized constructor of the class
+	 * @param message
+	 */
+	public MoodAnalyserTest(String message)
+	{
+        this.message = message;
+    }
+	/**
+	 * Default constructor
+	 */
+	public MoodAnalyserTest()
+	{
+        this.message = message;
+    }
+	/**
+	 * exceptionAnalyseMood is a function check if message contain sad then return 
+	 * if contain happy then return happy 
+	 * if contain null then also return happy 
+	 * @return string value
+	 */
+	public String exceptionAnalyseMood() 
+	{
+        try 
+        {
+        if (message.contains("sad"))
+            return "SAD";
+        return "HAPPY";
+        } 
+        catch (NullPointerException e) 
+        {
+            return "HAPPY";
+        }
     }
 }
